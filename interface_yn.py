665a3766a1e57185934cd52cad82d531cfd944be
Yorik Nyssen <yoriknyssen@MacBook-Pro-de-Yorik.local>
2023-11-27 19:32:18 +0100

    BDD fonctionne

--- interface_yn.py
+++ interface_yn.py
@@ -1,12 +1,41 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+db = SQLAlchemy() # db intitialized here
 app = Flask(__name__)
-@app.route('/')
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db.init_app(app)
 
-def index(): 
-    return render_template('index.html') 
-# no need to specify the name of the folder since we named it "templates"
+
+class Todo(db.Model) : 
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self): 
+        return '<Task %r>' %self.id
+    
+@app.route('/', methods= ['POST', 'GET'])
+
+def index():
+    if request.method == 'POST': 
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        
+        try: 
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except: 
+            return 'There was an issue adding your task'
+    
+    else : 
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks) 
+# no need to specify thexie name of the folder since we named it "templates"
 
 if __name__ == "__main__" : 
     app.run(debug= True)
-    
+     
