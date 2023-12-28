8ed7a730a2215c3180f88e0c44f0a5dd8cb98d68
maxime.richaudeau <maxime.richaudeau1@gmail.com>
2023-12-28 19:41:47 +0100

    Add api endpoints with environnement variables

---
+++ api.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+env = os.getenv("ENV")
+api_address = os.getenv(f"{env}_API_ADDRESS")
+
+
+shopping_list_api_endpoints = f'http://{api_address}:8000/recipe/get_shopping_list'
+database_api_endpoints = f'http://{api_address}:8000/database/'
