c7d1959aa1533ba8b50ba95331fb50a088bebc53
maxime.richaudeau <maxime.richaudeau1@gmail.com>
2023-12-28 19:42:29 +0100

    Add api request with fixed nb_persons and recipe_list

--- interface_yn.py
+++ interface_yn.py
@@ -1,10 +1,16 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from api import shopping_list_api_endpoints
+from flask_cors import CORS  # Import the CORS extension
+import requests
 
 # Initialisation of the datebase and the 
 db = SQLAlchemy() # db intitialized here
 app = Flask(__name__)
+
+CORS(app, origins=['*'])  # Initialize CORS with the Flask app
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
 db.init_app(app)
 
@@ -19,7 +25,7 @@ class Ingredients(db.Model) :
         return '<Task %r>' %self.id
 
 
-######### POST NEW #########    
+# ######### POST NEW #########    
 @app.route('/', methods= ['POST', 'GET'])
 def index():
     # if we want to upload a new task 
@@ -66,8 +72,29 @@ def update(id) :
     # if you click on the button update of the home page
     else :
         return render_template('update.html', task=task)
+    
+
+########### Get Ingredient list from list of recipe ##########   
+@app.route('/get_ingredient', methods=['POST', 'GET'])
+def get_ingredient(nb_pers=4, recipe_list=["pizza", "gateau au chocolat"]):
+    data = recipe_list
+    
+    # Endpoint to which the request will be forwarded
+    url = shopping_list_api_endpoints
+    
+    # Headers for the request
+    headers = {
+        'accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+
+    # Forward the request to the specified URL
+    response = requests.post(url,params={'nb_pers':nb_pers}, headers=headers, json=data)
+    
+    # Return the response back to the client
+    return response.json()
         
 
-if __name__ == "__main__" :  
+if __name__ == "__main__" :
     app.run(debug= True)
      
